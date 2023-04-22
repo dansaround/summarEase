8c4b9f95601d7165c5304dd7db6f2d933494050f
Dan <kcomtdaniel@gmail.com>
2023-04-22 13:19:15 -0500

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Header from "./components/Header";
 import AudioUploader from "./components/AudioUploader";
 import Output from "./components/Output";
 import Footer from "./components/Footer";
+import Navbar from "./components/NavBar";
 
 function App() {
   const [response, setResponse] = useState("");
 
   return (
     <div className="App">
+      <Navbar />
       <Header />
       <AudioUploader
         handleResponse={(res) => {
