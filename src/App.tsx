e80ebaa4e95907ad72423acb0c1759159d40e1ab
Ronald Prato <ronald.prato>
2023-04-22 13:20:06 -0500

    layout changed a bit

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,34 @@ import AudioUploader from "./components/AudioUploader";
 import Output from "./components/Output";
 import Footer from "./components/Footer";
 
+import "./index.css";
+
 function App() {
   const [response, setResponse] = useState("");
 
   return (
     <div className="App">
       <Header />
-      <AudioUploader
-        handleResponse={(res) => {
-          setResponse(res);
-        }}
-      />
+      <div className="main-contennt">
+        <div className="info-section">
+          <h2>Upload an audio file</h2>
+          <i>(.ogg or .mp3)</i>
+          <p>And get useful information about it:</p>
+          <ul>
+            <li>Transcription</li>
+            <li>Summary</li>
+            <li>Bullet points</li>
+            <li>Useful link references (if apply)</li>
+          </ul>
+        </div>
+
+        <AudioUploader
+          handleResponse={(res) => {
+            setResponse(res);
+          }}
+        />
+      </div>
+
       <Output response={response} />
       <Footer />
     </div>
